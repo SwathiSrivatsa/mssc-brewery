2d4e24d1a8a2b3ab59a677734bc623efb3a2c9f1
Swathi <swathibhatkuthyar@gmail.com>
2022-04-20 14:47:57 +0100

    First commit - spring project

---
+++ src/test/java/com/swa/microservices/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swa.microservices.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
